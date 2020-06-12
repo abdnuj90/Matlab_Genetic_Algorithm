d19d673653f0dcb276bd0a6872614dfb42a768c8
Abdullah Alnujaimi <abd_n90@live.com>
2020-06-13 00:43:10 +0300

    Changes to function 'ff' file
    More dicriptions are added to ff files to explain its role
    in the code. Previously, no description is given and user
    of the code can get confused.

--- ff.m
+++ ff.m
@@ -1,3 +1,6 @@
+% function to evaluate the binary value of a number and convert it into
+% string
+
 function [f,x1,x2] = ff(x)
     x1 = binaryVectorToDecimal(x([1:4]));
     x2 = binaryVectorToDecimal(x([5:8]));
